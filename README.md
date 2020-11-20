2e1055836d58ac9e8af95dac52e29e276906bfd6
birbie <birbie@protonmail.com>
2020-11-19 19:50:14 -0600

    script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # talk_scripts
 ISUsec talk script
+
+	chmod +x linux_tools.sh
+
+	./linux_tools.sh
---
+++ linux_tools.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt-get install nmap -y;
+sudo apt-get install file -y;
+sudo apt-get install grep -y;
+sudo apt-get install wireshark -y;
+sudo apt-get install strace -y;
+sudo apt-get install htop -y;
+sudo apt-get install whois -y;
+sudo apt-get install binutils -y;
